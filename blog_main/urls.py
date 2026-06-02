251f8050df6f722cf9fb08bd230bbff67cdf5421
Himanshu Shah <shahhimanshu9698@gmail.com>
2026-06-02 13:51:02 +0545

    Single blog page , assignment , search feature

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -1,27 +1,15 @@
-"""
-URL configuration for blog_main project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from .import views
 from django.contrib import admin
 from django.urls import path , include
 from django.conf.urls.static import static 
 from django.conf import settings 
+from blogs import views as blog_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('category/', include('blogs.urls')),
+    path('<slug:slug>/', blog_views.blog_detail, name='blog_detail'),
+    path('search/blog/', blog_views.search_blog, name='search_blog'),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render , redirect , get_object_or_404
 from django.http import HttpResponse
+from django.db.models import Q
+
 from .models import Blog  ,Category
 # Create your views here.
 def posts_by_category(request, category_id):
@@ -21,3 +23,31 @@ def posts_by_category(request, category_id):
     return render(request , 'posts_by_category.html' ,context )
     # print(posts)
     # return HttpResponse(posts)
+
+def blog_detail(request, slug):
+    try : 
+        post = Blog.objects.get(slug = slug, status = 'Publish')
+    except Blog.DoesNotExist:
+        return render(request, '404.html')
+    context= {
+        'post': post,
+    }
+
+    return render(request, 'blog_detail.html', context)
+
+def search_blog(request):
+    keyword = request.GET.get('keyword')
+    # print(keyword)
+    # search for the blogs matching the keyword in title or content
+    # and what if the keyword is empty then redirect to home page 
+    if keyword:
+        post = Blog.objects.filter(status= "Publish").filter(Q(title__icontains= keyword) | Q(author__username__icontains= keyword)| Q(short_description__icontains= keyword)| Q(category__category_name__icontains= keyword))
+    else:
+        return redirect('home')
+
+    context = {
+        'post': post,
+        'keyword': keyword,
+    }
+    
+    return render(request, 'search_results.html', context)
