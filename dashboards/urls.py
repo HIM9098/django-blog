d2b63057204e48c6c92dca94065641207519600e
Himanshu Shah <shahhimanshu9698@gmail.com>
2026-06-05 12:16:09 +0545

    added blogs in the post dashboard and did CRUD operations

--- dashboards/urls.py
+++ dashboards/urls.py
@@ -4,9 +4,19 @@ from dashboards import views
 
 urlpatterns= [
     path('', views.dashboard, name='dashboard'),
+
+    # Category URLS // CRUD operations
     path('category/' , views.category , name = "category"),
     path('category/edit_category/<int:id>/',views.edit_category, name = "edit_category"),
     path('category/delete_category/<int:id>/',views.delete_category, name = "delete_category"),
     path('category/add_category/', views.add_category, name = "add_category"),
+    
+    # Blog URLS // CRUD operations
+
+    path('blog/', views.blog, name = "blog_posts"),
+    path('blog/add_blog/', views.add_blog, name = "add_blog"),
+    path('blog/edit_blog/<int:id>/', views.edit_blog, name = "edit_blog"),
+    path('blog/delete_blog/<int:id>/', views.delete_blog, name = "delete_blog"),
+    
 
 ]
--- dashboards/views.py
+++ dashboards/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+# category CURD operations
 
 # lgoin checkup
 @login_required(login_url="login")
@@ -56,3 +57,71 @@ def delete_category(request, id):
     category = Category.objects.get(id=id)
     category.delete()
     return redirect("category")
+
+# blog CURD operations
+
+def blog(request):
+    blogs = Blog.objects.all()
+
+    context = {
+        "blogs": blogs,
+    }
+    return render(request, "dashboards/blog_posts.html", context)
+
+def delete_blog(request, id):   
+    blog = Blog.objects.get(id=id)
+    blog.delete()
+    return redirect("blog_posts")   
+
+def add_blog(request):
+    category = Category.objects.all()  # to show category in dropdown menu in add blog page
+    if request.method == "POST" and request.POST["title"] != "" and request.POST["short_description"] != "" and request.POST["blog_body"] != "" and request.FILES["featured_image"] != "":
+        title = request.POST["title"]
+        short_description = request.POST["short_description"]
+        blog_body = request.POST["blog_body"]
+        featured_image = request.FILES["featured_image"]
+        category_id = request.POST["category"]
+
+        category = Category.objects.get(id=category_id) # to get the category for assigining it to the blog 
+
+        blog = Blog.objects.create(
+            title=title,
+            short_description=short_description,
+            blog_body=blog_body,
+            featured_image=featured_image,
+            category=category,
+            author=request.user,
+            slug=title.replace(" ", "-").lower(),
+        )
+        blog.save()
+        return redirect("blog_posts")
+    else:
+        context = {"category": category}
+        return render(request, "dashboards/edit_post.html", context)
+    
+def edit_blog(request, id):
+    category = Category.objects.all() 
+    if request.method == "POST" and request.POST["title"] != "" and request.POST["short_description"] != "" and request.POST["blog_body"] != "":
+        blog = Blog.objects.get(id=id)
+        title = request.POST["title"]
+        short_description = request.POST["short_description"]
+        blog_body = request.POST["blog_body"]
+        category_id = request.POST["category"]
+
+        category = Category.objects.get(id=category_id) # to get the category for assigining it to the blog 
+
+        blog.title = title
+        blog.short_description = short_description
+        blog.blog_body = blog_body
+        blog.category = category
+        if "featured_image" in request.FILES: # checking if user has uploaded files or not 
+            featured_image = request.FILES["featured_image"]
+            blog.featured_image = featured_image
+
+        blog.save()
+        return redirect("blog_posts")
+    blog_post = Blog.objects.get(id=id)
+    context = {"category": category
+               ,"blog": blog_post
+               }
+    return render(request, "dashboards/edit.html",context)
